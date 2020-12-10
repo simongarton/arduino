773960c77a3659c40f11b745380a35e58c349434
Simon Garton <simon.garton@gmail.com>
2020-12-10 18:31:31 +1300

    Tidying up code.

--- dmd-life.c
+++ dmd-life.c
@@ -6,11 +6,9 @@
 #include "DMD.h"
 #include "TimerOne.h"
 #include "Arial_black_16.h"
-// you can remove the fonts if unused
 #include "SystemFont5x7.h"
 #define DISPLAYS_ACROSS 1
 #define DISPLAYS_DOWN 1
-/* change these values if you have more than one DMD connected */
 DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
 
 void ScanDMD()
@@ -21,13 +19,10 @@ void ScanDMD()
 void setup()
 {
   Timer1.initialize(5000);
-  /*period in microseconds to call ScanDMD. Anything longer than 5000 (5ms) and you can see flicker.*/
 
   Timer1.attachInterrupt(ScanDMD);
-  /*attach the Timer1 interrupt to ScanDMD which goes to dmd.scanDisplayBySPI()*/
 
   dmd.clearScreen(true);
-  /* true is normal (all pixels off), false is negative (all pixels on) */
 
   Serial.begin(9600);
 
@@ -179,7 +174,7 @@ void game(int w, int h)
 void loop()
 {
   iteration = iteration + 1;
-  game(28, 16); // this doesn't work for more than x = 28
+  game(28, 16); // this doesn't work for more than x = 28 - the display turns into noise.
   delay(delayLoop);
   delayLoop = delayLoop + 5 - random(0, 10);
   if (delayLoop < 0 || delayLoop > 500)
